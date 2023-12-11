9c7a93f5f2db66295701a60a14f02bbf7af79920
Roxanarhenals25 <roxanarhenals65@gmail.com>
2023-12-11 12:58:51 -0500

    feat: Render characters with additional details

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -4,18 +4,45 @@ import PropTypes from 'prop-types'
 function Characters(props) {
     const {characters} = props;
 
+    console.log(characters);
 
 return (
     <div className='characters'>
         <h1> Characters </h1>
-        <span className='back-home'> Back Home </span>
         <div className='container-characters'>
             {characters.map((character, index) => (
                 <div className='character-container' key={index}>
-                    <p>{character.name}</p>
+                    <div>
+                        <img src={character.image} alt={character.name} />
+                    </div>
+                    <div>
+                        <h3>{character.name}</h3>
+                        <h6>
+                            {character.status === "Alive" ? (
+                                <>
+                                <span className='alive'/>
+                                Alive
+                                </>
+                            ) : (
+                                <>
+                                <span className='dead'/>
+                                Dead
+                                </>
+                            )};
+                        </h6>
+                        <p>
+                            <span className='text-grey'> Episodes: </span>
+                            <span>{character.episode.length}</span>
+                        </p>
+                        <p>
+                            <span className='text-grey'> Species: </span>
+                            <span>{character.species}</span>
+                        </p>
+                    </div>
                 </div>
             ))}
         </div>
+        <span className='back-home'> Back Home </span>
     </div>
 )
 }
